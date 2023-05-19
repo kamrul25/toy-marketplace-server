0558b4a6ed6ad90f518445c77144ab0ae9acd8db
kamrul25 <kamrul.coder01@gmail.com>
2023-05-20 02:15:34 +0600

    create data

--- index.js
+++ index.js
@@ -24,7 +24,16 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+     await client.connect();
+
+     const toyCollection = client.db("toyMarketplace").collection("allToy")
+
+
+     app.post('/allToy', async(req, res) =>{
+      const toy = req.body;
+      const result = await toyCollection.insertOne(toy);
+      res.send(result)
+     })
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
