dbcafb513988cb10e1b5e77ee33ab7d58f129e7e
kamrul25 <kamrul.coder01@gmail.com>
2023-05-20 10:22:57 +0600

    try to slove vercel problem according to the blog but can not

--- index.js
+++ index.js
@@ -6,7 +6,14 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // middleware
-app.use(cors());
+const corsConfig = {
+  origin: '',
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE']
+}
+app.use(cors(corsConfig))
+app.options("", cors(corsConfig))
+
 app.use(express.json());
 
 // mongodb
@@ -24,7 +31,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+  //  await client.connect();
 
     const toyCollection = client.db("toyMarketplace").collection("allToy");
 
